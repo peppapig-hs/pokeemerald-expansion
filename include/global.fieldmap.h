112f07b8c70ad2d81158040f931dd0d1726213b5
PeppaPigHS <ptk.prasertsuk@gmail.com>
2021-12-24 20:22:57 +0800

    Overworld expansion

--- include/global.fieldmap.h
+++ include/global.fieldmap.h
@@ -61,8 +61,7 @@ struct BackupMapLayout
 struct ObjectEventTemplate
 {
     /*0x00*/ u8 localId;
-    /*0x01*/ u8 graphicsId;
-    /*0x02*/ u8 inConnection; // Leftover from FRLG
+    /*0x01*/ u16 graphicsId;
     /*0x04*/ s16 x;
     /*0x06*/ s16 y;
     /*0x08*/ u8 elevation;
@@ -187,8 +186,9 @@ struct ObjectEvent
              u32 disableJumpLandingGroundEffect:1;
              u32 fixedPriority:1;
              u32 hideReflection:1;
-    /*0x04*/ u8 spriteId;
-    /*0x05*/ u8 graphicsId;
+             u8 :0;
+    /*0x04*/ u8 spriteId:7;
+    /*0x05*/ u16 graphicsId:9;
     /*0x06*/ u8 movementType;
     /*0x07*/ u8 trainerType;
     /*0x08*/ u8 localId;
@@ -212,7 +212,7 @@ struct ObjectEvent
     /*0x20*/ u8 previousMovementDirection;
     /*0x21*/ u8 directionSequenceIndex;
     /*0x22*/ u8 playerCopyableMovement; // COPY_MOVE_*
-    /*size = 0x24*/
+    /*size = 0x25*/
 };
 
 struct ObjectEventGraphicsInfo
